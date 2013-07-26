8a299c49bcb8df1faac3b613b14a6478598c666c
Etienne <etienne.francois@esrf.fr>
2013-07-26 16:55:14 +0200

    FFT method for loop centering

--- lucid/centerId.py
+++ lucid/centerId.py
@@ -10,11 +10,14 @@ __copyright__ = "2013, ESRF"
 import opencv
 import cv
 import numpy
-import pyFAI.splitBBox
 import pylab
 import scipy.ndimage
 from toolbox import displayCv,white_detect,erode,dilate,open_image,smooth,imgInfo2cvImage
 import meshGen
+import Image
+import myutils as utils
+import scipy.stats
+from PyMca import SimpleMath
 
 
 #================================================================================#
@@ -47,53 +50,28 @@ def find_loop(imgInfo,showVisuals=False,zoom=0,testingProc = False,faceFindProc 
     #Test what type of image data has been given as argument "imgInfo"
     img = open_image("reference.png") 
     if isinstance(imgInfo,numpy.ndarray):       
-        #Treatment with a numpy array
-        numimg = numpy.fromstring(img.convert("L").tostring(),"uint8").astype("float32")    
-        numimg2 = imgInfo
-        numimg.shape = img.size[1],img.size[0]
-        c =  imgInfo.shape[0]//2,imgInfo.shape[1]//2
-
+        im = imgInfo
     elif isinstance(imgInfo,str):
         #Treatment with the path of the image
-        img2 = open_image(imgInfo)
-        numimg = numpy.fromstring(img.convert("L").tostring(),"uint8").astype("float32")
-        numimg2 = numpy.fromstring(img2.convert("L").tostring(),"uint8").astype("float32")
-        numimg.shape = img.size[1],img2.size[0]
-        numimg2.shape = img2.size[1],img2.size[0]
-        c =  img2.size[1]//2,img.size[0]//2
-
+        im = img2float(imgInfo)
     else:
         #Lauch TypeError exception if none type of image data supported is given
         raise TypeError("Unsupported type : Image path (str) or numpyarray (numpy.ndarray) needed")
 
-    #Azimutal integration
-    d0,d1 = numpy.ogrid[:img.size[1],:img.size[0]]
-    d0-=c[0]
-    d1-=c[1]
-    r=numpy.sqrt(d0**2+d1**2)
-    theta = numpy.arctan2(d1,d0)
-    dtheta = abs(scipy.ndimage.sobel(theta))
-    I,e0,e1,w,u=pyFAI.splitBBox.histoBBox2d(pos0=r,delta_pos0=numpy.ones(numimg.size),bins=(64,4),weights=numimg, pos1=theta, delta_pos1=dtheta)
-    Iflat = I.max(axis=0)
-    flat = numpy.interp(r.ravel(), e0, Iflat)
-    flat.shape=numimg2.shape
-    res = (255-numimg2)/(255-flat)
-    res2 = (255*(res.max()-res)/(res.max()-res.min())).clip(0,255).astype("uint8")
-
     #Choose between differents ways of launching. Priority order : Normal, Face finding, TestCase procedure.
     if testingProc == False and faceFindProc == False:
         if zoom>0:
-            return loop_detection(numimg2.astype("uint8"),showVisuals,zoom)
+            return loop_detection(im,showVisuals,zoom)
         else:
-            return loop_detection(res2,showVisuals,zoom)
+            return loop_detection(im,showVisuals,zoom)
     elif faceFindProc == True:
-        return face_detection(res2,0,zoom)
+        return face_detection(im,0,zoom)
     else:
         #TestCase procedure launcher : return format is different (imageWidth, imageHeight, [function common return])
         if zoom>0:
-            return (img2.size[0],img2.size[1],loop_detection(numimg2.astype("uint8"),zoom=zoom))
+            return (im.shape[1],im.shape[0],loop_detection(im,zoom=zoom))
         else:
-            return (img2.size[0],img2.size[1],loop_detection(res2,zoom=zoom))
+            return (im.shape[1],im.shape[0],loop_detection(im,zoom=zoom))
 
 
 # Explicit launcher for face finding with pretreatment of the image and the face detection
@@ -135,64 +113,186 @@ def loop_detection(imgInfo,showVisuals=False,zoom=0):
     :returns: (label,x,y) result from loop_detection function
 
     """
-    image=imgInfo2cvImage(imgInfo)
-    image2=cv.CloneImage(image)
+    im=imgInfo
+    im = im[1:-1,1:-2]
 
-    #if zoom == 0:
-    cv.AdaptiveThreshold(image2,image2,255,cv.CV_ADAPTIVE_THRESH_MEAN_C,cv.CV_THRESH_BINARY_INV,45,6)
-    #else:
-        #cv.Threshold(image2,image2,30,255,cv.CV_THRESH_BINARY)
+    #miror for edge effect
+    im = utils.expand(im,2,mode="mirror")
 
-    image3=cv.CloneImage(image2)
-    cv.Zero(image3)
-    storage = cv.CreateMemStorage()
-    contours = cv.FindContours(cv.CloneImage    
-    (image2),storage,cv.CV_RETR_EXTERNAL,cv.CV_CHAIN_APPROX_SIMPLE,(0,0))
+    #Low pass filter with FFT
+    shape = im.shape
+    mask = gaussian_mask((495,660),.5,.5,4)
 
-    cv.DrawContours(image3,contours,cv.RGB(255,255,255),cv.RGB (255,255,255),2,cv.CV_FILLED)
-    cv.Smooth(image3,image3,cv.CV_MEDIAN,11)
+    i1f = numpy.fft.fft2(im)
+    i2f = numpy.multiply(i1f,mask)
+    res = numpy.fft.ifft2(i2f)
+
+    #Sub the background
+    test = im-res.real
+
+    #Save img for checking
+    #pylab.imsave("/users/efrancoi/afterFFT.png",test)
+
+    #Sobel filter
+    imgnumpy = test
+
+    dx = scipy.ndimage.sobel(imgnumpy,axis=0,mode='reflect')  # horizontal derivative
+    dy = scipy.ndimage.sobel(imgnumpy,axis=1,mode='reflect')  # vertical derivative
+    mag = numpy.hypot(dx,dy)  # magnitude
+
+    #True img
+    mag = mag[2:-2,2:-2]
+
+    #threshold to get the edge found with sobel
+    mag=scipy.stats.threshold(mag,15,255)
+    mag=scipy.stats.threshold(mag,0,14,255)
+
+    #Noise deletion
+    mag = scipy.ndimage.morphology.binary_erosion(mag)
+    mag = scipy.ndimage.morphology.binary_erosion(mag)
+    mag = scipy.ndimage.morphology.binary_dilation(mag)
+    mag = scipy.ndimage.morphology.binary_dilation(mag)
+     
+    mag=numpy.uint8(mag)
+
+    #pylab.imsave("/users/efrancoi/essaiblabla2.png",mag,cmap=pylab.cm.Greys_r)
+
+    newImage = numpy.zeros_like(mag)
+    newImageTemp = numpy.zeros_like(mag)
+
+    #zone scan
+    n = (mag.shape)[1]-1
+    while n>=0:
+        if numpy.mean(mag[:,n])>0:
+            newImage[:,n]=0
+            break                         
+        n-=1
+    lst_seq = [[]]
+    seq_num=0
+    last_white = True
+    while n>=0:
+        nznp = numpy.nonzero(mag[:,n])[0]
+        if len(nznp>0):
+            last_white=True
+            lst_seq[seq_num].append((nznp[0],n))
+            lst_seq[seq_num].append((nznp[-1],n))
+        else:
+            if last_white == True:
+                lst_seq.append([])
+                seq_num+=1
+                last_white=False
+        n-=1
+    ans = map(len, lst_seq)
+    lst_final = lst_seq[ans.index(max(ans))]
     
-    image4 = cv.CloneImage(image3)
+    for points in lst_final:
+        newImage[points]=255
 
-    #check if the loop is in the image
-    count = white_detect(image4[10:image4.height-10,10:image4.width-10])
-    if count < 50:
+    n = (newImage.shape)[1]-1
+    while n>=0:
+        
+        if numpy.mean(newImage[:,n])>0:
+            mag[:,n+1:]=0
+            break                         
+        n-=1
+    
+    n=0
+    while n<((newImage.shape)[1]-1):
+        if numpy.mean(newImage[:,n])>0:
+            mag[:,:n]=0
+            break                         
+        n+=1
+
+
+
+    n = (mag.shape)[0]-1
+    while n>=0:
+        if numpy.mean(mag[n,:])>0:
+            newImage[n,:]=0
+            break                         
+        n-=1
+    lst_seq = [[]]
+    seq_num=0
+    last_white = True
+    while n>=0:
+        nznp = numpy.nonzero(mag[n,:])[0]
+        if len(nznp>0):
+            last_white=True
+            lst_seq[seq_num].append((n,nznp[0]))
+            lst_seq[seq_num].append((n,nznp[-1]))
+        else:
+            if last_white == True:
+                lst_seq.append([])
+                seq_num+=1
+                last_white=False
+        n-=1
+    ans = map(len, lst_seq)
+    lst_final = lst_seq[ans.index(max(ans))]
+
+    #print 'lst_seq 2', lst_final
+    
+    for points in lst_final:
+        newImage[points]=255
+        newImageTemp[points]=255
+
+    n = (newImageTemp.shape)[0]-1
+    while n>=0:
+        if numpy.mean(newImageTemp[n,:])>0:
+            mag[n+1:,:]=0
+            newImage[n+1:,:]=0
+            break                         
+        n-=1
+    
+    n=0
+    while n<((newImageTemp.shape)[0]-1):
+        if numpy.mean(newImageTemp[n,:])>0:
+            mag[:n,:]=0
+            newImage[:n,:]=0
+            break                         
+        n+=1
+    
+    #scan for profiling
+    lendif = []
+    n = (newImage.shape)[1]-1
+    while n>=0:
+        nznp = numpy.nonzero(mag[:,n])[0]
+        if len(nznp>0):
+            newImage[nznp[0]:(nznp[-1]+1),n]=255
+            lendif.append(nznp[-1]-nznp[0])
+        n-=1
+    nbPix = numpy.sum(newImage)/255
+    if nbPix<20:
         return ("No loop detected",-1,-1)
-    
-    #dilation and erosion if there is no zoom
-    if zoom == 0:
-        erode(image4,image4,11)
-        dilate(image4,image4,11)
 
-    count = white_detect(image4[10:image4.height-10,10:image4.width-10])
-    if count < 50:
-        (xcenter,ycenter) = find_approx_loop(image3)
-        if showVisuals==1:
-            cv.Line(image,(int(xcenter),int(0)),(int(xcenter),int(image.height)),cv.RGB(120.0,120.0,120.0))
-            cv.Line(image,(int(0),int(ycenter)),(int(image.width),int(ycenter)),cv.RGB(120.0,120.0,120.0))
-            displayCv(('Resultat',image),('ErosionDilation',image4),('AdaptiveThresh',image2),('FinalTreatmentVisualisation',image3))
-            cv.WaitKey(0)
-        return ('ApproxCoord',xcenter-15,ycenter)
-    if zoom == 0:
-        (xcenter,ycenter) = only_robot(image3)
-        if xcenter > 0:
-            if showVisuals:
-                cv.Line(image,(int(xcenter),int(0)),(int(xcenter),int(image.height)),cv.RGB(120.0,120.0,120.0))
-    	        cv.Line(image,(int(0),int(ycenter)),(int(image.width),int(ycenter)),cv.RGB(120.0,120.0,120.0))
-                displayCv(('Resultat',image),('ErosionDilation',image4),('AdaptiveThresh',image2),('FinalTreatmentVisualisation',image3))
-            return ("ArmRobot",xcenter,ycenter)
-    clean_the_robot(image3,image4)
-    adjust_detection(image3)
-    if zoom == 0:
-        (xcenter,ycenter) = calculate_center(image3,image3)
-    else:
-        (xcenter,ycenter) = calculate_center(image3,image3)
-    if showVisuals:
-        cv.Line(image,(int(xcenter),int(0)),(int(xcenter),int(image.height)),cv.RGB(120.0,120.0,120.0))
-        cv.Line(image,(int(0),int(ycenter)),(int(image.width),int(ycenter)),cv.RGB(120.0,120.0,120.0))  
-        displayCv(('Resultat',image),('ErosionDilation',image4),('AdaptiveThresh',image2),('FinalTreatmentVisualisation',image3))
-        cv.WaitKey(0)
-    return ("Coord",xcenter,ycenter)
+    #derivate and prifle smoothing
+    lendif2 = scipy.ndimage.gaussian_filter(lendif, sigma=15.0, order=0)
+    sm=SimpleMath.SimpleMath()
+    deriv=sm.derivate(range(len(lendif2)), lendif2)
+    deriv2 = scipy.ndimage.gaussian_filter(deriv[-1], sigma=3, order=0)
+    
+    #analize profile
+    centerValue = profileAnalyzer(deriv2)
+    
+    #get coord of center    
+    x=0
+    y=0
+    m=0
+    n = (newImage.shape)[1]-1
+    while n>=0:
+        nznp = numpy.nonzero(mag[:,n])[0]
+        if len(nznp>0):
+            if m==(centerValue):
+                newImage[:,n]=150
+                nznp = numpy.nonzero(mag[:,n])[0]
+                y = nznp[0]+(nznp[-1]-nznp[0])//2
+                newImage[y,:]=150
+                x = n
+            m+=1
+        n-=1
+
+    #pylab.imsave("/users/efrancoi/essaiblablamask.png",newImage,cmap=pylab.cm.Greys_r)
+
+    return ("Coord",int(x),int(y))
 
 #Detection of loop with the meshing systeme
 def find_loop_mesh(imgInfo,showVisuals=False,zoom=0,virtCenter=(-1,-1)):
@@ -298,212 +398,80 @@ def face_detection(imgInfo,showVisuals=False,zoom=0):
         cv.WaitKey(0)
     return count
 
-#Clean the robot part and keep just the loop
-def clean_the_robot(image,imageMask):
-    """|
-    
-    :param image : The image to modify
-    :type image : IplImage
-    :param imageMask : Image with morphological mathematic too clear the robot part
-    :type imageMask : IplImage
+def gaussian_mask(shape,sigma,sigma2,mulsigma):
 
-    """
-    i=imageMask.width-1
-    wbegin=0
-    while i>=1:
-        if white_detect(imageMask[:,i])>0:
-            wbegin = 1
-        else:
-            if wbegin==1:
-                cv.Set(image[:,0:i],0)
-                return i
-        i=i-1
-    cv.Set(image[:,0:1],0)
-    return -1
+    h = numpy.zeros(shape[0])
+    w = numpy.zeros(shape[1])
 
-#Calculate the center of the loop with the threated image
-def calculate_center(image,imageMask=None):
-    """|
+    h[(shape[0]//2)-(mulsigma*sigma):(shape[0]//2)+(mulsigma*sigma)]=1
+    w[(shape[1]//2)-(mulsigma*sigma2):(shape[1]//2)+(mulsigma*sigma2)]=1
 
-    :param image: treated image with just the loop for finding center
-    :type image: IplImage
+    b1 = scipy.ndimage.filters.gaussian_filter(h,sigma)
+    b2 = scipy.ndimage.filters.gaussian_filter(w,sigma)
 
-    :returns: Coordinates from the center in pixels (x,y)
-    """
-    i=0
-    left=-1
-    right=-1
-    up=-1
-    down=-1
-    if imageMask is None:
-        imageMask = image
-    while i<imageMask.width:
-        if white_detect(imageMask[:,i])>0:
-              left = i
-              break
-        i=i+1
+    h0 = numpy.empty_like(b1)
+    h1 = numpy.empty_like(b2)
 
-    while i<imageMask.width:
-        if white_detect(imageMask[:,i])==0:
-              right = i-1
-              break
-        i=i+1
+    h0[:shape[0] // 2] = b1[shape[0] - shape[0] // 2:]
+    h0[shape[0] // 2:] = b1[:shape[0] - shape[0] // 2]
+    h1[:shape[1] // 2] = b2[shape[1] - shape[1] // 2:]
+    h1[shape[1] // 2:] = b2[:shape[1] - shape[1] // 2]
 
-    if right == -1:
-        return (-1,-1)
+    g = numpy.outer(h0,h1)
+    return g
 
-    horizoncenter=right-round((right-left)/2.9)
-    j=3
-    
-    while j<image.height:
-        if image[j,horizoncenter]>0:
-              up = j
-              break
-        j=j+1
 
-    h=image.height-3
-    while h>j:
-        if image[h,horizoncenter]>0:
-              down = h
-              break
-        h=h-1
-    
-    if down == -1:
-        return (-2,-2)
-
-    verticalcenter = down - round((down-up)/2)
-    
-    return (horizoncenter,verticalcenter)
-
-#Approximate best coordinates for incomplete loops
-def find_approx_loop(image):
-    """|
-    
-    :param image : B/W Image with which is used for the approx
-    :type image : IplImage
-  
-    """
-    i=image.width-1
-    while i>=0:
-        if white_detect(image[:,i])>0:
-            j=image.height-1
-            while j>=0:
-                if image[j,i]>0:
-                    if white_detect(image[j-6:j+6,i-6:i+6])>17:
-                        return i,j
-                j=j-1
-        i=i-1
-    return -1,-1
-
-#Search if it's only the robot arm on the image and if it is, give better coordinates
-def only_robot(image3):
-    """|
-
-    :param image3 : B/W Image with which it is used
-    :type image3 : IplImage
-
-    """
-    xcenter=image3.width
-    ycenter=-1
-    i=image3.height-1
-
-    firstWhite = -1
-    lastWhite = -1
-    firstWhite2 = -1
-    lastWhite2 = -1
-
-    maxWhite=0
-    while i>=0:
-        count=white_detect(image3[i,:])
-        if count>0:
-            if firstWhite<0:
-                firstWhite=i
-            else:
-                if lastWhite>0:
-                    if firstWhite2<0:
-                        firstWhite2=i
-            if count>maxWhite:
-                maxWhite=count
-        else:
-            if firstWhite>0:
-                if lastWhite<0:
-                    lastWhite=i+1
-                else:
-                    if firstWhite2>0:
-                        if lastWhite2<0:
-                            lastWhite2=i+1            
-        i=i-1
-
-    if maxWhite < (image3.width*90)//100:
-        return (-1,-1)
-
-    if firstWhite2==0 or (firstWhite2>0 and lastWhite2<0):
-        lastWhite2=0
-    if firstWhite==0 or (firstWhite>0 and lastWhite<0):
-        lastWhite=0
-
-    mid = (firstWhite+lastWhite)//2
-    mid2 = (firstWhite2+lastWhite2)//2
-    if mid2 <0:
-        if mid > image3.height//2:
-            ycenter = (mid+image3.height)//2
-        else:
-            ycenter = mid//2
+def profileAnalyzer(deriv2):
+    slowCount = 0
+    maxValue = 0.0
+    centerValue = 20
+    ival=0
+    maxDeriv=0
+    firstIndex=0
+    littleLoop=-1
+    if float(len(deriv2[deriv2>0.5]))/float(len(deriv2))>0.4:        
+        while ival<len(deriv2):      
+            val = deriv2[ival]        
+            if val>0.5:
+                centerValue=ival
+                break
+            ival+=1
     else:
-        ycenter=(mid+mid2)//2
-
-    return (xcenter,ycenter)
-
-
-def adjust_detection(image):
-    """|
-
-    :param image : B/W Image with which it is used
-    :type image : IplImage
-
-    """
-    max_loop_white=0    
-    i=image.width-1
-    while i>=0:
-        up = -1
-        down = -1
-        j=0
-        while j<image.height:
-            if image[j,i]>0:
-                up = j
+        while ival<len(deriv2):      
+            val = deriv2[ival]        
+            if val>deriv2[(ival+1)%len(deriv2)] and val>deriv2[(ival+2)%len(deriv2)] and val>deriv2[(ival+3)%len(deriv2)] and val>deriv2[(ival+4)%len(deriv2)] and val>deriv2[(ival+5)%len(deriv2)]:
+                maxDeriv = val
                 break
-            j=j+1
-
-        h=image.height-3
-        while h>j:
-            if image[h,i]>0:
-              down = h
-              break
-            h=h-1
-        if((down-up<max_loop_white-round(max_loop_white*0.20)) and down-up>0):
-            #cv.Line(image,(int(i),int(0)),(int(i),int(image.height)),cv.RGB(120.0,120.0,120.0))        
-            break
-        if(down-up>max_loop_white):
-            max_loop_white = down-up
-        i=i-1
-    while i>=0:
-        up = -1
-        down = -1
-        j=0
-        while j<image.height:
-            if image[j,i]>0:
-                up = j
+            ival+=1
+        firstIndex = ival
+        if firstIndex==0:
+            firstIndex=1
+        while ival<len(deriv2):     
+            val = deriv2[ival]
+            if val<0.3 and val>0:
+                slowCount+=1        
+            elif val<=0.0:
+                maxValue = ival
+                #print deriv2[ival]
+                if numpy.mean(deriv2[ival:ival+ival])<0.0:
+                    centerValue = maxValue
+                    break
+            else:
+                slowCount=0
+            #print (ival-slowCount)-firstIndex,";",slowCount,";",ival
+            if (slowCount >= (ival-slowCount)/2) and maxDeriv>0.3:    
+                centerValue=ival-slowCount
                 break
-            j=j+1
+            elif (ival-slowCount)-firstIndex<=0 and float(len(deriv2))/float(slowCount+firstIndex)<8 and maxDeriv<0.3 and littleLoop<0:
+                littleLoop=firstIndex
+            ival+=1
+        #print littleLoop,max(deriv2[:centerValue])
+        if littleLoop>0 and max(deriv2[:centerValue])>2:
+            centerValue = littleLoop
+    return centerValue
 
-        h=image.height-3
-        while h>j:
-            if image[h,i]>0:
-              down = h
-              break
-            h=h-1
-        if(down-up>max_loop_white):
-            #cv.Line(image,(int(i),int(0)),(int(i),int(image.height)),cv.RGB(120.0,120.0,120.0))  
-            return cv.Set(image[:,0:i],0)
-        i=i-1
-    return -1,-1
+def img2float(fn):
+    im = Image.open(fn)
+    im2=im.convert("F");nim = numpy.fromstring(im2.tostring(),dtype="float32")
+    nim.shape = im.size[1],im.size[0]
+    return nim
